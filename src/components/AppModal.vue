fdcdcb2b35e4806e9936bfa138c42035c843a631
hoangthaiduong2001 <duongtantien1@gmail.com>
2025-07-26 20:27:16 +0700

    add css for modal

--- src/components/AppModal.vue
+++ src/components/AppModal.vue
@@ -17,4 +17,44 @@
 export default {};
 </script>
 
-<style></style>
+<style>
+.modal {
+  position: fixed;
+  z-index: 1;
+  left: 0;
+  top: 0;
+  display: flex;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.4);
+}
+
+.modal__container {
+  width: 950px;
+  margin: auto;
+  margin-top: 70px;
+  background: white;
+  border: 1px solid #888;
+  padding: 10px 20px;
+}
+
+.modal__header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.modal__close {
+  width: 50px;
+  height: 50px;
+  line-height: 50px;
+  border-radius: 50%;
+  text-align: center;
+  font-size: 30px;
+}
+
+.modal__close:hover {
+  background-color: rgba(0, 0, 0, 0.2);
+  cursor: pointer;
+}
+</style>
